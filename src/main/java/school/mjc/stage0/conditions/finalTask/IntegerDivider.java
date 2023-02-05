68c62e61ad52ec8e39ec60a341140a6960fd044a
Anastasiya P <anastasiapatsei@gmail.com>
2023-02-05 15:25:10 +0200

    solved

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int result = 0;
+        if (divider == 0){
+            System.out.println("division by zero");
+            return;
+        } else{
+            result = (dividend / divider) * divider;
+        }
+        if(result == dividend){
+            System.out.println("can be divided completely");
+        } else{
+            System.out.println("cannot be divided completely");
+
+        }
     }
 }
